6e499b9092358ccc52acb851471f6aa360646759
204g1a0555 <204g1a0555@srit.ac.in>
2024-06-03 15:09:39 +0530

    added

---
+++ firstvs.java
@@ -0,0 +1,10 @@
+class firstvs{
+    int n;
+    public firstvs(){
+System.out.println("Hello World");
+    }
+    public static void main(String args[]){
+        System.out.println("Hello world");
+        Object o = new firstvs();
+    }
+}
